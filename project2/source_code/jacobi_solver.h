ec31bc1ea4b49b6562ff9db54442b45bc17b5602
Justin Ngo <phongbilly456@gmail.com>
2023-04-30 15:29:27 -0400

    jacobi chunking not working

--- project2/source_code/jacobi_solver.h
+++ project2/source_code/jacobi_solver.h
@@ -12,6 +12,24 @@ typedef struct matrix_s {
     float *elements;
 }  matrix_t;
 
+/* Thread data structure */
+typedef struct thread_data_s {
+	int tid;
+	int num_threads;
+	matrix_t A;
+	matrix_t B;
+	matrix_t *x;
+	matrix_t *new_x;
+	int max_iter;
+	int start_index;
+	int end_index;
+	pthread_barrier_t *barrier;
+	pthread_mutex_t *lock;
+	int *converged;
+	float *diff;
+	int *num_iter;
+} thread_data_t;
+
 /* Function prototypes */
 matrix_t allocate_matrix(int, int, int);
 extern void compute_gold(const matrix_t, matrix_t, const matrix_t, int);
@@ -19,6 +37,7 @@ extern void display_jacobi_solution(const matrix_t, const matrix_t, const matrix
 int check_if_diagonal_dominant(const matrix_t);
 matrix_t create_diagonally_dominant_matrix(int, int);
 void compute_using_pthreads_v1(const matrix_t, matrix_t, const matrix_t, int max_iter, int num_threads);
+void *jacobi_v1(void *args);
 void compute_using_pthreads_v2(const matrix_t, matrix_t, const matrix_t, int max_iter, int num_threads);
 void print_matrix(const matrix_t);
 float get_random_number(int, int);
