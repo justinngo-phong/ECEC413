a69c120f23cf9532d66915f83da9bca01a2d3447
Justin Ngo <phongbilly456@gmail.com>
2023-05-07 19:05:57 -0400

    gaussian not working

--- project3/source_code/gauss_eliminate.h
+++ project3/source_code/gauss_eliminate.h
@@ -8,5 +8,18 @@ typedef struct {
     float* elements;            /* Pointer to the first element of the matrix */
 } Matrix;
 
+typedef struct thread_data_s {
+
+int tid;
+int num_threads;
+int start_index;
+int end_index;
+int matrix_size;
+Matrix U; 
+pthread_barrier_t *barrier;
+pthread_mutex_t *lock;
+
+}thread_data_t;
+
 #endif /* _MATRIXMUL_H_ */
 
